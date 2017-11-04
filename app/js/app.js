be96564f763fe40929b33a2d26ed0a128e2d9b2f
Stiletto <pankratov.symbyanz@bk.ru>
2017-11-04 23:03:29 +0300

    ... online v

--- app/js/app.js
+++ app/js/app.js
@@ -24,7 +24,7 @@ angular
   ])
 
 .factory('socket', function ($rootScope) {
-  var socket = io.connect('http://192.168.0.121:3000/'); // url ... https://infinite-lowlands-52249.herokuapp.com/
+  var socket = io.connect('https://infinite-lowlands-52249.herokuapp.com/'); // url ... http://192.168.0.121:3000/
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
